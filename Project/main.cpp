17bf9befb197276583df5e951f0821876141dd7f
alessia266 <s297038@studenti.polito.it>
2024-06-25 17:41:46 +0200

    completamento codice

--- Project/main.cpp
+++ Project/main.cpp
@@ -2,10 +2,13 @@
 #include <fstream>
 #include "Utils.hpp"
 #include "DFN.hpp"
+#include "UCDUtilities.hpp"
+
 
 using namespace std;
 using namespace DFNLibrary;
 using namespace Eigen;
+using namespace Gedim;
 
 int main()
 {
@@ -44,6 +47,49 @@ int main()
 
     if (!ExportFractTraces(fracture))
         return 4;
+
+    if (!CalculateSubPolygons(fracture,
+                              trace,
+                              e))
+        return 5;
+
+    if (!ExportPolygonalMesh(fracture))
+        return 6;
+
+
+    //exporting fractureId 0 in paraview
+    auto fracture0 = fracture.Polygons[0];
+    string filepath = "./";
+    MatrixXd point = MatrixXd::Zero(3, size(fracture0.IdCoord0D));
+    MatrixXi segments = MatrixXi::Zero(2, size(fracture0.IdEdg1D));
+
+    unsigned int c = 0;
+    for(map<unsigned int, Vector3d>::iterator it = fracture0.IdCoord0D.begin();
+         it != fracture0.IdCoord0D.end(); it++)
+    {
+        for (unsigned int r = 0; r < 3; r++)
+        {
+            point(r,c) = it->second[r];
+        }
+        c++;
+    }
+    unsigned int edg = 0;
+    for(map<unsigned int, Vector2i>::iterator it1 = fracture0.IdEdg1D.begin();
+         it1 != fracture0.IdEdg1D.end(); it1++)
+    {
+        segments(0, edg) = it1->second[0];
+        segments(1, edg) = it1->second[1];
+        edg++;
+    }
+
+
+    Gedim::UCDUtilities exporter;
+
+    exporter.ExportPoints(filepath + "/Geometry0Ds.inp",
+                          point);
+    exporter.ExportSegments(filepath + "/Geometry1Ds.inp",
+                            point,
+                            segments);
 
     return 0;
 }
--- Project/src/DFN.hpp
+++ Project/src/DFN.hpp
@@ -11,9 +11,10 @@ namespace DFNLibrary{
 
 struct Intersections
 {
-    vector<Vector3d> IntersectionCoord = {};
-    bool Tips;
-    double Length;
+    vector<Vector3d> IntersectionCoord = {}; //coordinates intersections between fracture end trace direction
+    vector<unsigned int> sideId = {}; //marker
+    bool Tips;  //passante/non passante
+    double Length; //trace length
 };
 
 struct SortingTraces
@@ -22,6 +23,7 @@ struct SortingTraces
     bool Tips;
     double Length;
 
+    //operator to define the order in type SortingTrace
     friend bool operator>(const SortingTraces& s1, const SortingTraces& s2)
     {
         if (s1.Tips == false && s2.Tips == true) return true;
@@ -29,7 +31,40 @@ struct SortingTraces
         return (s1.Length > s2.Length);
     }
 
+    // friend bool operator<=(const SortingTraces& s1, const SortingTraces& s2)
+    // {
+    //     if (s1.Tips == false && s2.Tips == true) return false;
+    //     if (s1.Tips == true && s2.Tips == false) return true;
+    //     return (s1.Length <= s2.Length);
+    // }
 };
+
+
+struct Utils
+{
+    unsigned int DimToIgnore;  //dimension to ignore in projection in 2D
+    Vector3d Norm;  //normal vector to the fracture
+};
+
+
+struct PolygonalMesh
+{
+    //cell0D
+    unsigned int NumberCell0D = 0;
+    map<unsigned int, Vector3d> IdCoord0D = {};  //map: {id coordinate, Coordinate}
+
+    //cell1D
+    unsigned int NumberCell1D = 0;
+    map<unsigned int, Vector2i> IdEdg1D = {};  //map: {id edge, id coordinates}
+
+
+    //cell2D
+    unsigned int NumberCell2D = 0;
+    map<unsigned int, vector<unsigned int>> VerticesCell2D = {};  //map: {id polygon, id vertices}
+    map<unsigned int, vector<unsigned int>> EdgesCell2D = {};   //map: {id polygon, id edges}
+
+};
+
 
 
 struct Fractures
@@ -39,8 +74,10 @@ struct Fractures
     map<unsigned int, Vector3d>  MinFract = {}; //map: {ID, {smallest coordinates x, y, z}}
     map<unsigned int, Vector3d>  MaxFract = {}; //map: {ID, {biggest coordinates x, y, z}}
     vector<Vector2i> IDFracturesComparable ={};
-    map <unsigned int, map<unsigned int, Intersections>> IntersectionsFracture = {}; //map {IDFracture, {{IDTrace, {{{coord}}, {Marker}, Tips, length}}}}
-    map<unsigned int, vector<SortingTraces>> FractureSortTraces = {}; //map {IDFracture, sorted vector of traces}
+    map <unsigned int, Utils> UtilsID = {}; //map: {ID, dimension to ignore in fracture projection}
+    map <unsigned int, map<unsigned int, Intersections>> IntersectionsFracture = {}; //map {IDFracture, {{IDTrace, {{{coord}}, sideId, Tips, length}}}}
+    map<unsigned int, vector<SortingTraces>> FractureSortTraces = {}; //map {IDFracture, sorted vector of struct SortingTrace}
+    map<unsigned int, PolygonalMesh> Polygons = {}; //map {ID fracture, struct PolygonalMesh}
 };
 
 
@@ -52,3 +89,6 @@ struct Traces
 };
 
 }
+
+
+
